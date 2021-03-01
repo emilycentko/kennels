9e36e72e103efe62d15c7be21a2635e5562788c4
emilycentko <emily.centko@gmail.com>
2021-03-01 14:32:25 -0600

    Edited address tag in Animal.js

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -4,7 +4,7 @@ import "./Animal.css"
 export const AnimalCard = ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <address className="location__address">Location: {location.name}</address>
-        <p className="animal__owner">Customer: {customer.name}</p>
+        <div className="location__address">Location: {location.name}</div>
+        <div className="animal__owner">Customer: {customer.name}</div>
     </section>
 )
