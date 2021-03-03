581672927c5f7139a84241bb0b62a1dde009d7ee
emilycentko <emily.centko@gmail.com>
2021-03-03 11:34:48 -0600

    Refactored Employee Card to add hyperlink that links to more info about employee expanded with location data

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -13,6 +13,7 @@ import { AnimalForm } from "./animal/AnimalForm"
 import { EmployeeForm } from "./employee/EmployeeForm"
 import { LocationForm } from "./location/LocationForm"
 import { AnimalDetail } from "./animal/AnimalDetail"
+import { EmployeeDetail } from "./employee/EmployeeDetail"
 
 export const ApplicationViews = () => {
     return (
@@ -65,6 +66,12 @@ export const ApplicationViews = () => {
                         <EmployeeForm />
                     </Route>
                 </LocationProvider>
+            </EmployeeProvider>
+
+            <EmployeeProvider>
+                <Route exact path="/employees/detail/:employeeId(\d+)">
+		            <EmployeeDetail />
+	            </Route>
             </EmployeeProvider>
 
             {/* Render the customers list when http://localhost:3000/customers */}
